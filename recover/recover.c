def1ddb3d132e91b777f91a1a67179d7d180c674
Wil Trahan <wiltrahan@intracitygeeks.org>
2018-04-07 16:35:12 +0000

    begin recover

---
+++ recover/recover.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
+
+    char *infile = argv[1];
+    FILE *inptr = fopen(infile, "r");
+
+    if(inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+}
