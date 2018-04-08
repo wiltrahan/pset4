d24845ee2e9b5283522d586cc57eff2bd6f662ed
Wil Trahan <wiltrahan@intracitygeeks.org>
2018-04-08 21:01:10 +0000

    continue recover

--- recover/recover.c
+++ recover/recover.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     }
 
     char *infile = argv[1];
-    FILE *inptr = fopen(infile, "r");
+    FILE *inptr = fopen("card.raw", "r");
 
     if(inptr == NULL)
     {
