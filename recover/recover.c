4af20fd1f8b4ad98620d9c2493e4eef55cb197f6
Wil Trahan <wiltrahan@intracitygeeks.org>
2018-04-11 01:59:03 +0000

    complete recover

--- recover/recover.c
+++ recover/recover.c
@@ -25,27 +25,38 @@ int main(int argc, char *argv[])
 
     unsigned char buffer[512];
     char jpgName[8];
+    FILE *img = fopen(jpgName, "w");
 
     //read 512 bytes into a buffer
     while(fread(buffer, 512, 1, inptr)){
 
         //is it the start a jpg
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0){
-            //if yes...
-                //check if already found jpg
-                    //if no
-                        //then open to write the file name
-                        sprintf(jpgName, "%03i.jpg", picsFound);
-                        picsFound++;
-                        //FILE *img = fopen(filename, "w");
-                    //if yes
-                        //close previous file, open new one
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
 
-           }
+            //if yes...
+            //check if already found jpg
+            //if yes
+            //close previous file, open new one
+            if(img != NULL)
+            {
+                fclose(img);
+            }
+            //if no
+            //then open to write the file name
+            sprintf(jpgName, "%03i.jpg", picsFound);
+            //increment pics found to update jpg name
+            picsFound++;
+            img = fopen(jpgName, "w");
+            fwrite(buffer, sizeof(char), 512, img);
+        }
+        else
+        {
+            fwrite(buffer, sizeof(char), 512, img);
+        }
 
     }
     //close any remaining files
-
-
-
+    fclose(img);
+    fclose(inptr);
 }
